47aef77c66871f472f024a1b8c7b33faf0d6852f
森谷　仁美 <hitomi_moritani@jmas.co.jp>
2021-03-29 21:05:50 +0900

    最初のコミット

---
+++ eplusReader/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  eplusReader
+//
+//  Created by 森谷仁美 on 2021/03/29.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
