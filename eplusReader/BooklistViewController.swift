fa8ec53420d8426ce8f4daa58240f37d426fd13f
森谷　仁美 <hitomi_moritani@jmas.co.jp>
2021-04-01 14:25:52 +0900

    利用規約　仮完成 画面遷移設定前

---
+++ eplusReader/BooklistViewController.swift
@@ -0,0 +1,29 @@
+//
+//  BooklistViewController.swift
+//  eplusReader
+//
+//  Created by 森谷仁美 on 2021/03/31.
+//
+
+import UIKit
+
+class BooklistViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+    
+
+    /*
+    // MARK: - Navigation
+
+    // In a storyboard-based application, you will often want to do a little preparation before navigation
+    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+        // Get the new view controller using segue.destination.
+        // Pass the selected object to the new view controller.
+    }
+    */
+
+}
---
+++ eplusReader/File.swift
@@ -0,0 +1,8 @@
+//
+//  File.swift
+//  eplusReader
+//
+//  Created by 森谷仁美 on 2021/04/01.
+//
+
+import Foundation
