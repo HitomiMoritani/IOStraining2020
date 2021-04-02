2846f70241cde8dc39ddb80811990b818a88fe74
森谷　仁美 <hitomi_moritani@jmas.co.jp>
2021-04-02 10:49:33 +0900

    2　起動画面　レビュー後修正１（修正内容：画像の差し替え） 2　利用規約　レビュー前

--- eplusReader/ViewController.swift
+++ eplusReader/ViewController.swift
@@ -8,12 +8,20 @@
 import UIKit
 
 class ViewController: UIViewController {
-
+//画面をロードした瞬間にして欲しいことを書く（＋＠画面終了時にしたい処理も他にあるよ）
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view.
     }
-
-
+    
+    @IBAction func tap(_ sender: Any) {
+        //まずは、違うstororyboardであることをここで定義します（フロア指定。１階）
+        let anotherStoryboard:UIStoryboard = UIStoryboard(name: "List", bundle: nil)
+        //どのviewかを指定（部屋番号を指定。101号室的な）
+        let anotherViewController: UIViewController = anotherStoryboard.instantiateViewController(withIdentifier:"List")
+        //画面遷移が実行
+        self.present(anotherViewController, animated: true, completion: nil)
+        
+    }
 }
 
